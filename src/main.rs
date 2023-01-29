4c70ce9ded9c949a7cfdecfa30846aa79389b9c6
pekpuglia <pepuglia@gmail.com>
2023-01-29 16:59:11 -0300

    analog reading test

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,29 @@
 #![no_std]
 #![no_main]
 
-mod car;
 
 use panic_halt as _;
 
-use car::{Car, motor::Motor};
+mod car;
+use car::{Car, ControllableCar};
 
-use arduino_hal::{
-    delay_ms,
-    simple_pwm::{IntoPwmPin, Prescaler, Timer0Pwm, Timer1Pwm, Timer2Pwm},
-};
+mod ir_sensor;
 
-// fn motor_profile(motor: &mut impl ControllableMotor, count: u32) {
-//     let rem = count.rem(768);
-//     match rem {
-//         0..=255 => motor.set_state(MotorState::Forward(rem as u8)),
-//         256..=511 => motor.set_state(MotorState::Backward((rem - 256) as u8)),
-//         _ => motor.set_state(MotorState::Stopped),
-//     }
-//     arduino_hal::delay_ms(20);
-// }
+use arduino_hal::{delay_ms};
 
 #[arduino_hal::entry]
 fn main() -> ! {
-    //colocar como globais?
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let timer0 = Timer0Pwm::new(dp.TC0, Prescaler::Prescale64);
-    let timer1 = Timer1Pwm::new(dp.TC1, Prescaler::Prescale64);
-    let timer2 = Timer2Pwm::new(dp.TC2, Prescaler::Prescale64);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    let mut car = Car::default();
+    let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
 
-    let mut count = 0;
+    let a0 = pins.a0.into_analog_input(&mut adc);
+
     loop {
-        // motor_profile(&mut motor, count);
-        // count += 1;
-        // motor.set_state(MotorState::Forward(128));
-        // delay_ms(500);
-        // motor.set_state(MotorState::Forward(255));
-        // delay_ms(500);
-        // motor.set_state(MotorState::Backward(200));
-        // delay_ms(500);
-        // motor.set_state(MotorState::Stopped);
-        // delay_ms(500);
+        let val = a0.analog_read(&mut adc);
+        ufmt::uwriteln!(&mut serial, "Medida: {}", val).unwrap();
+        delay_ms(500);
     }
 }
